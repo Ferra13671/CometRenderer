2ed08c4086af0ebb78eb1a801ad8e77f291704a5
Ferra <ferra13671@gmail.com>
2025-08-22 18:27:42 +0300

    main

---
+++ src/testmod/java/com/ferra13671/cometrenderer/test/TestMod.java
@@ -0,0 +1,10 @@
+package com.ferra13671.cometrenderer.test;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
