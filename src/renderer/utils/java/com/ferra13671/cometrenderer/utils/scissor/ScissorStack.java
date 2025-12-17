5702edf886ad1901a86518dc9bc6cc4704a164bf
Ferra13671 <ferra13671@gmail.com>
2025-12-17 17:54:54 +0300

    Refactor ScissorStack

--- src/renderer/utils/java/com/ferra13671/cometrenderer/utils/scissor/ScissorStack.java
+++ src/renderer/utils/java/com/ferra13671/cometrenderer/utils/scissor/ScissorStack.java
@@ -1,51 +1,20 @@
 package com.ferra13671.cometrenderer.utils.scissor;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import com.ferra13671.ferraguard.annotations.OverriddenMethod;
+
 import java.util.Objects;
+import java.util.Stack;
 
-/**
- * Стек для областей, используемых ножницами.
- */
-public class ScissorStack {
-    /** Стек. **/
-    private final Deque<ScissorRect> stack = new ArrayDeque<>();
-    /** Текущая область ножниц. **/
-    private ScissorRect current;
+public class ScissorStack extends Stack<ScissorRect> {
 
-    /**
-     * Увеличивает указатель стека на единицу и устанавливает новую область ножниц.
-     *
-     * @param rect новая область ножниц.
-     */
-    public void push(ScissorRect rect) {
+    @Override
+    @OverriddenMethod
+    public ScissorRect push(ScissorRect rect) {
         rect = rect.fixRect();
         ScissorRect scissorRect = rect;
-        if (current != null)
-            scissorRect = Objects.requireNonNullElse(rect.intersection(current), new ScissorRect(0, 0, 0, 0));
-        stack.addLast(scissorRect);
-        current = scissorRect;
-    }
+        if (!isEmpty())
+            scissorRect = Objects.requireNonNullElse(rect.intersection(peek()), new ScissorRect(0, 0, 0, 0));
 
-    /**
-     * Возвращает текущую область ножниц.
-     *
-     * @return текущая область ножниц.
-     */
-    public ScissorRect current() {
-        return current;
-    }
-
-    /**
-     * Уменьшает указатель стека на единицу и удаляет последнюю область ножниц.
-     */
-    public void pop() {
-        //Если стек пустой, кидаем исключение
-        if (stack.isEmpty()) {
-            throw new IllegalStateException("Scissor stack underflow");
-        } else {
-            stack.removeLast();
-            current = stack.peekLast();
-        }
+        return super.push(scissorRect);
     }
 }
