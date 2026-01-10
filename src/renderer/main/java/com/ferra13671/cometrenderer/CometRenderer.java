285c0d9f9a396a03a883f09cffac4fb0acb59125
Ferra13671 <ferra13671@gmail.com>
2026-01-10 04:27:05 +0300

    Update README.md

--- src/renderer/main/java/com/ferra13671/cometrenderer/CometRenderer.java
+++ src/renderer/main/java/com/ferra13671/cometrenderer/CometRenderer.java
@@ -128,7 +128,7 @@ public class CometRenderer {
     }
 
     private static void initRegistry() {
-        registry.setImmutable(CometTags.COMET_RENDERER_VERSION, "2.1.1");
+        registry.setImmutable(CometTags.COMET_RENDERER_VERSION, "2.2");
 
         String vendor = GL11.glGetString(GL11.GL_VENDOR);
         String version = GL11.glGetString(GL11.GL_VERSION);
