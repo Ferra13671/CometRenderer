f92d5ea06e753893043a89533a75352664cf453d
Ferra13671 <ferra13671@gmail.com>
2026-01-15 17:51:41 +0300

    [minecraft] Change example structure

---
+++ minecraft/general/java/com/ferra13671/cometrenderer/plugins/minecraft/testing/UIRenderTest.java
@@ -0,0 +1,93 @@
+package com.ferra13671.cometrenderer.plugins.minecraft.testing;
+
+import com.ferra13671.cometrenderer.CometRenderer;
+import com.ferra13671.cometrenderer.plugins.minecraft.RectColors;
+import com.ferra13671.cometrenderer.plugins.minecraft.RenderColor;
+import com.ferra13671.cometrenderer.plugins.minecraft.drawer.IDrawer;
+import com.ferra13671.cometrenderer.plugins.minecraft.drawer.impl.*;
+import com.ferra13671.gltextureutils.ColorMode;
+import com.ferra13671.gltextureutils.GLTexture;
+import com.ferra13671.gltextureutils.TextureFiltering;
+import com.ferra13671.gltextureutils.TextureWrapping;
+import com.ferra13671.gltextureutils.atlas.TextureBorder;
+import com.ferra13671.gltextureutils.builder.GLTextureInfo;
+import com.ferra13671.gltextureutils.loader.TextureLoader;
+import com.ferra13671.gltextureutils.loader.TextureLoaders;
+
+import java.awt.*;
+
+public final class UIRenderTest {
+    private static final TextureLoader<String> textureLoader = new TextureLoader<>() {
+        @Override
+        public GLTextureInfo load(String s, ColorMode colorMode) throws Exception {
+            return TextureLoaders.INPUT_STREAM.load(UIRenderTest.class.getClassLoader().getResourceAsStream(s), colorMode);
+        }
+    };
+
+    private static GLTexture texture;
+    public static IDrawer standaloneDrawer;
+
+    public static void init() {
+        texture =
+                textureLoader.createTextureBuilder()
+                        .name("Test-texture")
+                        .info("texture.jpg")
+                        .filtering(TextureFiltering.SMOOTH)
+                        .wrapping(TextureWrapping.DEFAULT)
+                        .build();
+
+        standaloneDrawer = new BasicTextureDrawer()
+                .setTexture(texture)
+                .rectSized(1200, 400, 100, 100, new TextureBorder(0, 0, 1, 1))
+                .build()
+                .makeStandalone();
+    }
+
+    public static void draw() {
+        drawOneColorRect();
+        drawMultiColorRect();
+        drawTextures();
+        drawRoundedRects();
+    }
+
+    private static void drawOneColorRect() {
+        new BasicRectDrawer(() -> CometRenderer.setShaderColor(RenderColor.of(Color.RED).toVector4f()))
+                .rectSized(800, 400, 100, 100)
+                .build()
+                .tryDraw()
+                .close();
+    }
+
+    private static void drawMultiColorRect() {
+        new ColoredRectDrawer()
+                .rectSized(800, 510, 100, 100, new RectColors(
+                        RenderColor.of(1f, 1f, 1f, 1f),
+                        RenderColor.of(1f, 0f, 0f, 1f),
+                        RenderColor.of(0f, 1f, 0f, 1f),
+                        RenderColor.of(0f, 0f, 1f, 1f)
+                ))
+                .build()
+                .tryDraw()
+                .close();
+    }
+
+    private static void drawTextures() {
+        standaloneDrawer.tryDraw();
+
+        new RoundedTextureDrawer()
+                .setTexture(texture)
+                .rectSized(1200, 510, 100, 100, 20, RectColors.oneColor(RenderColor.WHITE), new TextureBorder(0, 0, 1, 1))
+                .build()
+                .tryDraw()
+                .close();
+    }
+
+    private static void drawRoundedRects() {
+        new RoundedRectDrawer()
+                .rectSized(1000, 400, 100, 100, 20, RectColors.oneColor(RenderColor.of(Color.MAGENTA)))
+                .rectSized(1000, 510, 100, 100, 20, RectColors.horizontalGradient(RenderColor.of(Color.BLUE), RenderColor.of(Color.CYAN)))
+                .build()
+                .tryDraw()
+                .close();
+    }
+}
--- minecraft/minecraft-1_21_11-fabric/src/example/java/com/ferra13671/cometrenderer/test/TestMod.java
+++ minecraft/minecraft-1_21_11-fabric/src/example/java/com/ferra13671/cometrenderer/test/TestMod.java
@@ -2,51 +2,19 @@ package com.ferra13671.cometrenderer.test;
 
 import com.ferra13671.cometrenderer.CometRenderer;
 import com.ferra13671.cometrenderer.plugins.minecraft.MinecraftPlugin;
-import com.ferra13671.cometrenderer.plugins.minecraft.RectColors;
-import com.ferra13671.cometrenderer.plugins.minecraft.RenderColor;
-import com.ferra13671.cometrenderer.plugins.minecraft.drawer.IDrawer;
 import com.ferra13671.cometrenderer.plugins.minecraft.drawer.impl.*;
+import com.ferra13671.cometrenderer.plugins.minecraft.testing.UIRenderTest;
 import com.ferra13671.cometrenderer.test.mixins.IGlGpuBuffer;
-import com.ferra13671.gltextureutils.ColorMode;
-import com.ferra13671.gltextureutils.GLTexture;
-import com.ferra13671.gltextureutils.TextureFiltering;
-import com.ferra13671.gltextureutils.TextureWrapping;
-import com.ferra13671.gltextureutils.atlas.TextureBorder;
-import com.ferra13671.gltextureutils.builder.GLTextureInfo;
-import com.ferra13671.gltextureutils.loader.TextureLoader;
-import com.ferra13671.gltextureutils.loader.TextureLoaders;
 
 import java.awt.*;
 
 public class TestMod implements Mc {
 
-    private static final TextureLoader<String> textureLoader = new TextureLoader<>() {
-        @Override
-        public GLTextureInfo load(String s, ColorMode colorMode) throws Exception {
-            return TextureLoaders.INPUT_STREAM.load(TestMod.class.getClassLoader().getResourceAsStream(s), colorMode);
-        }
-    };
-    private static GLTexture texture;
-
-    public static IDrawer standaloneDrawer;
-
     public static void initRender() {
         CometRenderer.init();
         MinecraftPlugin.init(glGpuBuffer -> ((IGlGpuBuffer) glGpuBuffer)._getId(), () -> 1);
 
-        texture =
-                textureLoader.createTextureBuilder()
-                        .name("Test-texture")
-                        .info("texture.jpg")
-                        .filtering(TextureFiltering.SMOOTH)
-                        .wrapping(TextureWrapping.DEFAULT)
-                        .build();
-
-        standaloneDrawer = new BasicTextureDrawer()
-                .setTexture(texture)
-                .rectSized(1200, 400, 100, 100, new TextureBorder(0, 0, 1, 1))
-                .build()
-                .makeStandalone();
+        UIRenderTest.init();
     }
 
     public static void render() {
@@ -55,53 +23,6 @@ public class TestMod implements Mc {
 
         MinecraftPlugin.getInstance().bindMainFramebuffer(true);
 
-        drawOneColorRect();
-
-        drawMultiColorRect();
-
-        drawTextures();
-
-        drawRoundedRects();
-    }
-
-    private static void drawOneColorRect() {
-        new BasicRectDrawer(() -> CometRenderer.setShaderColor(RenderColor.of(Color.RED).toVector4f()))
-                .rectSized(800, 400, 100, 100)
-                .build()
-                .tryDraw()
-                .close();
-    }
-
-    private static void drawMultiColorRect() {
-        new ColoredRectDrawer()
-                .rectSized(800, 510, 100, 100, new RectColors(
-                        RenderColor.of(1f, 1f, 1f, 1f),
-                        RenderColor.of(1f, 0f, 0f, 1f),
-                        RenderColor.of(0f, 1f, 0f, 1f),
-                        RenderColor.of(0f, 0f, 1f, 1f)
-                ))
-                .build()
-                .tryDraw()
-                .close();
-    }
-
-    private static void drawTextures() {
-        standaloneDrawer.tryDraw();
-
-        new RoundedTextureDrawer()
-                .setTexture(texture)
-                .rectSized(1200, 510, 100, 100, 20, RectColors.oneColor(RenderColor.WHITE), new TextureBorder(0, 0, 1, 1))
-                .build()
-                .tryDraw()
-                .close();
-    }
-
-    private static void drawRoundedRects() {
-        new RoundedRectDrawer()
-                .rectSized(1000, 400, 100, 100, 20, RectColors.oneColor(RenderColor.of(Color.MAGENTA)))
-                .rectSized(1000, 510, 100, 100, 20, RectColors.horizontalGradient(RenderColor.of(Color.BLUE), RenderColor.of(Color.CYAN)))
-                .build()
-                .tryDraw()
-                .close();
+        UIRenderTest.draw();
     }
 }
