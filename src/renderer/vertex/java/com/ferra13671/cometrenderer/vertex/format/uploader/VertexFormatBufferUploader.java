78482517a9c24fbe2e91636de7b17343d2424d34
Ferra13671 <ferra13671@gmail.com>
2026-01-16 17:20:28 +0300

    [main] Little changes

--- src/renderer/vertex/java/com/ferra13671/cometrenderer/vertex/format/uploader/VertexFormatBufferUploader.java
+++ src/renderer/vertex/java/com/ferra13671/cometrenderer/vertex/format/uploader/VertexFormatBufferUploader.java
@@ -4,8 +4,6 @@ import com.ferra13671.cometrenderer.buffer.GpuBuffer;
 import com.ferra13671.cometrenderer.vertex.format.VertexFormat;
 import com.ferra13671.cometrenderer.vertex.format.VertexFormatBuffer;
 
-import java.util.HashMap;
-
 /**
  * Установщик формата вершины к буфферу вершин.
  */
