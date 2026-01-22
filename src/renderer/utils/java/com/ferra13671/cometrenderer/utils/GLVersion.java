0cd2fd761bce7648c4b54345c0d8940b6b599075
Ferra13671 <ferra13671@gmail.com>
2026-01-22 16:14:45 +0300

    [main] Change GLVersion id's

--- src/renderer/utils/java/com/ferra13671/cometrenderer/utils/GLVersion.java
+++ src/renderer/utils/java/com/ferra13671/cometrenderer/utils/GLVersion.java
@@ -4,25 +4,25 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum GLVersion {
-    GL10("1.0", null, 0),
-    GL11("1.1", null, 1),
-    GL12("1.2", null, 2),
-    GL13("1.3", null, 3),
-    GL14("1.4", null, 4),
-    GL15("1.5",null, 5),
-    GL20("2.0", "110", 6),
-    GL21("2.1", "120", 7),
-    GL30("3.0", "130", 8),
-    GL31("3.1", "140", 9),
-    GL32("3.2", "150", 10),
-    GL33("3.3", "330 core", 11),
-    GL40("4.0", "400 core", 12),
-    GL41("4.1", "410 core", 13),
-    GL42("4.2", "420 core", 14),
-    GL43("4.3", "430 core", 15),
-    GL44("4.4", "440 core", 16),
-    GL45("4.5", "450 core", 17),
-    GL46("4.6", "460 core", 18);
+    GL10("1.0", null, 10),
+    GL11("1.1", null, 11),
+    GL12("1.2", null, 12),
+    GL13("1.3", null, 13),
+    GL14("1.4", null, 14),
+    GL15("1.5",null, 15),
+    GL20("2.0", "110", 20),
+    GL21("2.1", "120", 21),
+    GL30("3.0", "130", 30),
+    GL31("3.1", "140", 31),
+    GL32("3.2", "150", 32),
+    GL33("3.3", "330 core", 33),
+    GL40("4.0", "400 core", 40),
+    GL41("4.1", "410 core", 41),
+    GL42("4.2", "420 core", 42),
+    GL43("4.3", "430 core", 43),
+    GL44("4.4", "440 core", 44),
+    GL45("4.5", "450 core", 45),
+    GL46("4.6", "460 core", 46);
 
     public final String glVersion;
     public final String glslVersion;
