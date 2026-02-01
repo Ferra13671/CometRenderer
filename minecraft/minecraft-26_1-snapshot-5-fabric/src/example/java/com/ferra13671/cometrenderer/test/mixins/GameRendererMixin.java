e242ceca2822e60d12a297c1d5735e52500f6242
Ferra13671 <ferra13671@gmail.com>
2026-02-01 13:29:52 +0300

    [minecraft] Port to 26.1-snapshot-5

---
+++ minecraft/minecraft-26_1-snapshot-5-fabric/src/example/java/com/ferra13671/cometrenderer/test/mixins/GameRendererMixin.java
@@ -0,0 +1,23 @@
+package com.ferra13671.cometrenderer.test.mixins;
+
+import com.ferra13671.cometrenderer.plugins.minecraft.AbstractMinecraftPlugin;
+import com.ferra13671.cometrenderer.test.TestMod;
+import com.mojang.blaze3d.opengl.GlStateManager;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/render/state/GuiRenderState;reset()V", shift = At.Shift.AFTER))
+    public void modifyRenderBeforeGui(DeltaTracker deltaTracker, boolean tick, CallbackInfo ci) {
+        AbstractMinecraftPlugin.getInstance().setupUIProjection();
+        GlStateManager._disableDepthTest();
+
+        TestMod.render();
+    }
+}
