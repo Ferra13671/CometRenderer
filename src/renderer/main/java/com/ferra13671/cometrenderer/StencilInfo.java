76856f938b622143c1638756d9eeec941e2ac5a6
Ferra13671 <ferra13671@gmail.com>
2026-06-10 19:30:43 +0300

    [main] Change default StencilInfo values

--- src/renderer/main/java/com/ferra13671/cometrenderer/StencilInfo.java
+++ src/renderer/main/java/com/ferra13671/cometrenderer/StencilInfo.java
@@ -11,14 +11,21 @@ import org.apiguardian.api.API;
 @Builder
 @API(status = API.Status.EXPERIMENTAL, since = "2.9")
 public record StencilInfo(Boolean stencilMask, Boolean depthMask, ColorMask colorMask, StencilFunction func, StencilOp op) {
-    public static final StencilInfo DEFAULT_WRITE = StencilInfo.builder()
+    public static final StencilInfo WRITE = StencilInfo.builder()
             .stencilMask(true)
             .depthMask(false)
             .colorMask(new ColorMask(false, false, false, false))
             .func(new StencilFunction(AlphaFunction.ALWAYS, 1, 255))
             .op(new StencilOp(StencilOpAction.KEEP, StencilOpAction.KEEP, StencilOpAction.REPLACE))
             .build();
-    public static final StencilInfo DEFAULT_READ = StencilInfo.builder()
+    public static final StencilInfo READ = StencilInfo.builder()
+            .stencilMask(false)
+            .depthMask(true)
+            .colorMask(new ColorMask(true, true, true, true))
+            .func(new StencilFunction(AlphaFunction.EQUAL, 1, 255))
+            .op(new StencilOp(StencilOpAction.KEEP, StencilOpAction.KEEP, StencilOpAction.KEEP))
+            .build();
+    public static final StencilInfo INVERTED_READ = StencilInfo.builder()
             .stencilMask(false)
             .depthMask(true)
             .colorMask(new ColorMask(true, true, true, true))
