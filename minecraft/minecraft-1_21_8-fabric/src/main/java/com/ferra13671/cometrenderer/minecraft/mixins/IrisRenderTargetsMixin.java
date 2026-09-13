c8b28e97002692aabab7019921db572e3b805592
Ferra13671 <ferra13671@gmail.com>
2026-09-13 19:05:42 +0300

    [crm] Fix incompatibility with Iris Shaders for versions 1.21.5+ (due to the stencil buffer).

---
+++ minecraft/minecraft-1_21_8-fabric/src/main/java/com/ferra13671/cometrenderer/minecraft/mixins/IrisRenderTargetsMixin.java
@@ -0,0 +1,18 @@
+package com.ferra13671.cometrenderer.minecraft.mixins;
+
+import com.mojang.blaze3d.textures.TextureFormat;
+import net.irisshaders.iris.targets.RenderTargets;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Pseudo
+@Mixin(RenderTargets.class)
+public class IrisRenderTargetsMixin {
+
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/GpuDevice;createTexture(Ljava/lang/String;ILcom/mojang/blaze3d/textures/TextureFormat;IIII)Lcom/mojang/blaze3d/textures/GpuTexture;"), index = 2)
+    public TextureFormat modifyArgInCreateTextureInInit(TextureFormat textureFormat) {
+        return textureFormat == null ? TextureFormat.DEPTH32 : textureFormat;
+    }
+}
